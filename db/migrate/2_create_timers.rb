b67527462b61870d17c912360d6a749ffc471990
mkrlearns <contact@ryanmeek.com>
2020-07-03 18:22:40 +0000

    user & timer db migrations

---
+++ db/migrate/2_create_timers.rb
@@ -0,0 +1,10 @@
+class CreateTimers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :timers do |t|
+      t.integer :seconds
+      t.integer :remaining
+      t.boolean :running
+      t.integer :user_id
+    end
+  end
+end
