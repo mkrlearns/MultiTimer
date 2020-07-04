5cb7c27fac164af700de07a107f8a880219e18da
ryan m <contact@ryanmeek.com>
2020-07-04 18:45:28 +0000

    timers controller: new, edit, delete

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,3 +1,20 @@
 class TimersController < ApplicationController
 
+  post '/timer' do
+    timer = Timer.new(seconds: seconds, running: false, remaining: seconds, started: Time.new.utc)
+    user = current_user
+    user.timers << timer
+    redirect '/' if user.save
+  end
+
+  patch '/timer/:id/edit' do
+    timer = Timer.find_by_id(params[:id])
+    redirect '/' if timer.update(seconds: seconds, remaining: seconds)
+  end
+
+  delete '/timer/:id/delete' do
+    timer = Timer.find_by_id(params[:id])
+    redirect '/' if timer.destroy
+  end
+
 end
