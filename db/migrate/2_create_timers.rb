c00d8418dadf8fa9b5738b85e1d5f344f5785526
ryan m <contact@ryanmeek.com>
2020-07-04 16:50:58 +0000

    add started as datetime for timers

--- db/migrate/2_create_timers.rb
+++ db/migrate/2_create_timers.rb
@@ -2,6 +2,7 @@ class CreateTimers < ActiveRecord::Migration[6.0]
   def change
     create_table :timers do |t|
       t.integer :seconds
+      t.datetime :started
       t.integer :remaining
       t.boolean :running
       t.integer :user_id
