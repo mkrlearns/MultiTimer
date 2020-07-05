ad225c4bbfb52b3bcc13315cb95f10c886792208
ryan m <contact@ryanmeek.com>
2020-07-05 10:43:50 +0000

    seconds calc

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,6 +1,11 @@
 class TimersController < ApplicationController
 
+  def get_seconds(minutes = 0, seconds = 0)
+    (minutes.to_i * 60) + seconds.to_i
+  end
+
   post '/timer' do
+    seconds = get_seconds(params[:minutes], params[:seconds])
     timer = Timer.new(seconds: seconds, running: false, remaining: seconds, started: Time.new.utc)
     user = current_user
     user.timers << timer
@@ -8,6 +13,7 @@ class TimersController < ApplicationController
   end
 
   patch '/timer/:id/edit' do
+    seconds = get_seconds(params[:minutes], params[:seconds])
     timer = Timer.find_by_id(params[:id])
     redirect '/' if timer.update(seconds: seconds, remaining: seconds)
   end
