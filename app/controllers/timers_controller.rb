b5fcdf52471edf6960796492745ecb40a14ae197
ryan m <contact@ryanmeek.com>
2020-07-05 14:36:23 +0000

    timer toggle & reset

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -22,4 +22,21 @@ class TimersController < ApplicationController
     redirect '/' if timer.destroy
   end
 
+  patch '/timer/:id/toggle' do
+    timer = Timer.find_by_id(params[:id])
+    if timer.running
+      time_passed = Time.new.utc.to_i - timer.started.to_i
+      timer.update(remaining: timer.remaining - time_passed)
+    else
+      timer.update(started: Time.new.utc)
+    end
+    timer.update(running: !timer.running)
+    redirect '/'
+  end
+
+  patch '/timer/:id/reset' do
+    timer = Timer.find_by_id(params[:id])
+    redirect '/' if timer.update(remaining: timer.seconds, started: Time.new.utc, running: false)
+  end
+
 end
